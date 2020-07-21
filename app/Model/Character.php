9cf4191ce91100373f28b7b2da45562cc6a480dd
Anderson Lairton <lairton.anderson@gmail.com>
2020-07-21 10:46:06 -0300

    definindo fillable no model character

--- app/Model/Character.php
+++ app/Model/Character.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Character extends Model
 {
-    //
+    protected $table ='characters';
+    public $timestamps=true;
+    protected $fillable = ['frase_characters','characters_description','characters_name'];
 }
