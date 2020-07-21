1975786db85041a3405385d59e822479b86ab6e0
Anderson Lairton <lairton.anderson@gmail.com>
2020-07-20 21:09:33 -0300

    criando migrations das tabelas characters e images com as respectivas chaves estrangeiras

---
+++ database/migrations/2020_07_20_231802_create_characters_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCharactersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('characters', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('characters_name',255)->unique();
+            $table->string('frase_characters',100);
+            $table->string('characters_description',255);
+
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('characters');
+    }
+}
