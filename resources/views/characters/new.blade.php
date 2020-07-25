6856afd204b9504f4fb0a3445e06985981c7dfc7
Anderson Lairton <lairton.anderson@gmail.com>
2020-07-25 18:31:13 -0300

    selecionando no formulario o game a qual o personagem pertence

--- resources/views/characters/new.blade.php
+++ resources/views/characters/new.blade.php
@@ -9,19 +9,19 @@
         <div class="card">
             <div class="card-header">
                 @if(empty($character))
-                    Novo
+                Novo
                 @else
-                    Editar {{$character->characters_name}}
+                Editar {{$character->characters_name}}
                 @endif
             </div>
             @if(count($errors)>0)
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach($errors->all() as $error)
-                            <li>{{$error}}</li>
-                        @endforeach
-                    </ul>
-                </div>
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                    @endforeach
+                </ul>
+            </div>
             @endif
 
 
@@ -35,35 +35,48 @@
 
                 <div class="col-md-12 col-sm-12">
                     <label>Frase:</label>
-                    <input type="text" class="form-control"  name="frase_characters" value="{{!empty($character->frase_characters)?$character->frase_characters:old('frase_characters')}}">
+                    <input type="text" class="form-control" name="frase_characters" value="{{!empty($character->frase_characters)?$character->frase_characters:old('frase_characters')}}">
                 </div>
 
                 <div class="col-md-12 col-sm-12">
                     <label>Descrição:</label>
                     <textarea class="form-control" name="characters_description" rows="3">{{!empty($character->characters_description)?$character->characters_description:old('characters_description')}}</textarea>
-                    
+
                 </div>
 
                 <div class="col-md-12 col-sm-12">
                     <label>Game:</label>
-                    <input type="text" class="form-control" name="game" value="{{!empty($character->game)?$character->game:old('game')}}">
+                    <select name="game" class="form-control" id="character_game">
+                        <option value="empty">Selecione o Game</option>
+                        @foreach($games as $g)
+                        <option id="{{$g->id}}" value="{{$g->id}}">{{$g->game_name}}</option>
+                        @endforeach
+                    </select>
                 </div>
 
 
                 <div class="col-md-12 col-sm-12">
-                    <label >Imagem</label>
-                    <input type="file" name="image" class="form-control">
+                    <label>Imagem</label>
+                    <input type="file" name="image" class="form-control" value="{{!empty($character->image)?$character->image:old('image')}}">
                 </div>
-                
+
                 <div class="col-md-12 col-sm-12">
-                    <label ></label>
+                    <label></label>
                     <button type="submit" class="btn btn-success">Salvar</button>
                 </div>
-                
+
 
             </form>
 
         </div>
     </div>
 </body>
+<script type="text/javascript">
+    let j = "{{empty($character)}}";
+
+    if (j != 1) {
+        let i = document.getElementById("{{!empty($character->game)?$character->game:''}}");
+        i.setAttribute('selected', 'selected');
+    }
+</script>
 @endsection
